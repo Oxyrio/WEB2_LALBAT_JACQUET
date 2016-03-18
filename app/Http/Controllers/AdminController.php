4224a5cd8f892dd0702d7c48278da0995868a61e
Aymé Jacquet <axelandayme@hotmail.fr>
2016-03-18 11:50:43 +0100

    Controller again

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,3 +8,35 @@ use App\Models\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+    public function index()
+    {
+        $projets = Post::All();
+        return view('admin.index')->with(compact('projets'));
+    }
+
+    public function projets()
+    {
+        $projets = Post::All();
+        return view('admin.projets')->with(compact('projets'));
+    }
+
+    public function articles()
+    {
+        $articles = Article::All();
+        return view('admin.articles')->with(compact('articles'));
+    }
+
+    public function users()
+    {
+        $users = User::All();
+        return view('admin.users')->with(compact('users'));
+    }
+}
