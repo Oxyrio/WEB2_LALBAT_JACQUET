c633aba4048645440ffe1aaff4b06459d7d5a1c7
Aymé Jacquet <axelandayme@hotmail.fr>
2016-03-18 12:44:00 +0100

    Ajout des factories commentaires et projet

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -28,3 +28,35 @@ $factory->define(App\Models\Article::class, function (Faker\Generator $faker) {
         'textarticle' => $faker->text,
     ];
 });
+
+$factory->define(App\Models\Comment::class, function (Faker\Generator $faker) {
+    return [
+        'commentaire' => $faker->text(150),
+        'article_id' => $faker->numberBetween(1,10),
+        'user_id' => $faker->numberBetween(1,10),
+    ];
+});
+
+$factory->define(App\Models\Post::class, function (Faker\Generator $faker) {
+    return [
+
+        'user_id' => $faker->numberBetween(1,10),
+        'titleprojet' => $faker->title,
+        'commanditaire' => $faker->name,
+        'command_metier' => $faker->company,
+        'command_tel' => $faker->phoneNumber,
+        'command_mail' => $faker->email,
+        'command_ville' => $faker->address,
+        'decisionnaire' => $faker->name,
+        'decid_metier' => $faker->company, // Pas de faker job i guess...
+        'decid_tel' => $faker->phoneNumber,
+        'decid_mail' => $faker->email,
+        'decid_ville' => $faker->address,
+        'type_projet' => $faker->text(150),
+        'contexte' => $faker->text(150),
+        'objectifs' => $faker->text(150),
+        'textprojet' => $faker->text(150),
+        'contraintes' => $faker->text(150),
+        'statut' => $faker->text(),
+    ];
+});
